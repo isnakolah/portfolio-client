def17811c85f29ac4e621590779a1ac39ad90a43
Daniel Nakolah <nakolahdaniel1@gmail.com>
2021-05-01 01:31:43 +0300

    Introduce Lazy loading

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
+import { CircularProgress } from "@material-ui/core";
 
 // styles
 import "fontsource-roboto";
-import Layout from "./components/layout/Layout";
+// import Layout from "./components/layout/Layout";
+const Layout = lazy(() => import("./components/layout/Layout"));
 
 const App = () => {
-  return <Layout />;
+  return (
+    <Suspense fallback={<CircularProgress />}>
+      <Layout />
+    </Suspense>
+  );
 };
 
 export default App;
