a9e3885560e735c679876bd695cd464259069d4e
Daniel Nakolah <nakolahdaniel1@gmail.com>
2021-05-11 16:19:39 +0300

    constants for the background colors

---
+++ src/utils/colors.js
@@ -0,0 +1,11 @@
+export const pageBackground =
+  "linear-gradient(to right top, rgb(101 223 201), rgb(108 219 235))";
+
+export const mainAreaBackground =
+  "linear-gradient(to right bottom,rgba(255, 255, 255, 0.7),rgba(255, 255, 255, 0.3))";
+
+export const circleBackground =
+  "linear-gradient(to right bottom,rgba(255, 255, 255, 0.8),rgba(255, 255, 255, 0.3))";
+
+export const navLinkBackground =
+  "linear-gradient(to right, rgba(101, 223, 201, 80%), rgba(186, 235, 242, 0%))";
