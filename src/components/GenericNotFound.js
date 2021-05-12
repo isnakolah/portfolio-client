1334fef35423fdfc2631bd0ab7e74d093a394da3
Daniel Nakolah <nakolahdaniel1@gmail.com>
2021-05-13 01:46:26 +0300

    Create the not found page

--- src/components/GenericNotFound.js
+++ src/components/GenericNotFound.js
@@ -1,10 +1,45 @@
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Grid,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
 import React from "react";
 
+import notFound from "../assets/images/not-found.png";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    background: "none",
+    boxShadow: "none",
+    "& img": {
+      width: "auto",
+      height: "80%",
+    },
+    "& h4": {
+      padding: "0",
+    },
+  },
+}));
+
 const GenericNotFound = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <p>Not found page</p>
-    </div>
+    <Grid
+      container
+      justify="center"
+      alignContent="center"
+      component={Card}
+      className={classes.root}
+    >
+      <CardMedia component="img" src={notFound} alt="something went wrong" />
+      <CardContent component={Typography} variant="h4">
+        404. Page Not Found.
+      </CardContent>
+    </Grid>
   );
 };
 
