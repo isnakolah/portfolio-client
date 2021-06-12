563787280791647ef9c68fa4939584929f7dc6a2
Daniel Nakolah <nakolahdaniel1@gmail.com>
2021-06-12 22:04:34 +0300

    EasterEggs

---
+++ src/components/EasterEggs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Typography, Grid, Box } from "@material-ui/core"
+
+const EasterEggs = () => {
+  return (
+    <Grid container alignItems="center" justify="center" component={Box} height={1}>
+      <Typography variant="h3">
+        I love you Candy
+      </Typography>
+    </Grid>
+  )
+}
+
+export default EasterEggs
